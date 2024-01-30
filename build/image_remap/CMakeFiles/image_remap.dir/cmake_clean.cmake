ff0d80c1214fa9ce9db47c1b05e80d1265010c0c
ZiweiSong96 <627114348@qq.com>
2024-01-30 20:05:51 +0800

    20240130

---
+++ build/image_remap/CMakeFiles/image_remap.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_remap.dir/src/image_remap.cpp.o"
+  "CMakeFiles/image_remap.dir/src/image_remap.cpp.o.d"
+  "image_remap"
+  "image_remap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_remap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/image_remap/CMakeFiles/image_remap_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_remap_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/image_remap_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
